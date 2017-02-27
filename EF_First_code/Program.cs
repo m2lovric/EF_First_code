9a74e45bc8603287c277c5887faab88f80efc454
matteo4 <matteo4@MATTEO>
2017-02-27 21:01:39 +0100

    Add project files.

---
+++ EF_First_code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_First_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
